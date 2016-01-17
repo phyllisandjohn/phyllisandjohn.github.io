221814640105cdec59792fb282b7ba1850fa3c6e
phyllisandjohn <phyllisandjohn.guestlist@gmail.com>
2016-01-17 05:09:31 +0100

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,5 @@
 var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('4df56842935f4881bf3fa7f6592fed99');
+spotifyApi.setAccessToken('b64ac87cd207424ab87eb317b9a25aa5');
 
 // get Elvis' albums, using Promises through Promise, Q or when
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
