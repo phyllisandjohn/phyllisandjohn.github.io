d70d6ec24553c3c63eb80f0403b1553c4733ade6
phyllisandjohn <phyllisandjohn.guestlist@gmail.com>
2016-01-17 18:44:19 +0100

    Update mail.php

--- request/mail.php
+++ request/mail.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST["submit"])){
   $message = $_POST['msg'];
-  mail("phyllisandjohn.guestlist@gmail.com", "Song Request", $message, "");
+  mail("phyllisandjohn.guestlist@gmail.com", "Song Request", $message, "From: kirstenrauffer@gmail.com");
 }
 ?>
