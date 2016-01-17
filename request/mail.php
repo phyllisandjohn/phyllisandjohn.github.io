55b8e890835fc046d58f2ac22852979e936ad627
phyllisandjohn <phyllisandjohn.guestlist@gmail.com>
2016-01-17 18:45:39 +0100

    Update mail.php

--- request/mail.php
+++ request/mail.php
@@ -1,6 +1,9 @@
 <?php
 if(isset($_POST["submit"])){
-  $message = $_POST['msg'];
+  $message = $_POST['song'];
+  $email = 'kirstenrauffer@gmail.com'
+  $headers = 'From:'. $email . "\r\n"; // Sender's Email
+  $headers .= 'Cc:'. $email . "\r\n"; // Carbon copy to Sender
   mail("phyllisandjohn.guestlist@gmail.com", "Song Request", $message, "From: kirstenrauffer@gmail.com");
 }
 ?>
