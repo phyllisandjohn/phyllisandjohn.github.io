3a225d33bfc32d849b01331777386365c44858b7
phyllisandjohn <phyllisandjohn.guestlist@gmail.com>
2016-01-17 05:24:16 +0100

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,8 @@
 angular
-  .module('exampleApp', ['spotify'])
+  .module('requestApp', ['spotify'])
   .config(function (SpotifyProvider) {
     SpotifyProvider.setClientId('b64ac87cd207424ab87eb317b9a25aa5');
-    SpotifyProvider.setRedirectUri('http://example.com/callback.html');
+    SpotifyProvider.setRedirectUri('http://phyllisandjohn.com/callback.html');
     SpotifyProvider.setScope('playlist-read-private');
   })
   .controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
